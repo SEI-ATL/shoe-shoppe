39bfd8880d84f9ab944d34d4089062a25baef48e
frankcollins3 <fwc3rd@gmail.com>
2020-12-07 19:07:16 -0500

    [nba-api] get res.render route working

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 
 const express = require("express")
+const axios = require("axios")
+const ejsLayouts = require("express-ejs-layouts");
 const app = express()
+const path = require("path")
 
-//home screen test for now.
+//middleware
+app.use(express.urlencoded({ extended: false })) 
+app.use(express.json()) // this function parses incoming data.
+app.use(express.static("views"))
+
+app.set("view engine", "ejs") //use a templating engine (ejs)
+app.use(ejsLayouts)
+
+//home screen test route for now.
 app.get("/", (req, res) => {
-  res.send("My first express web page!!!!")
-})
+  res.render("index.ejs");
+});
 
 //ROUTE TEST for the login and signup page. this might actually be the first page. 
-app.get("/login", (req, res) => {
-  res.send("ROUTE TEST LOGIN AND SIGNUP")
-})
+app.get("/auth/login", (req, res) => {
+  res.render("login.ejs");
+});
 
 //store all the player data here.
 app.get("/profile", (req, res) => {
-  res.send("store the saved teams here")
+  res.render("profile.ejs");
+});
+
+app.listen(3000, () => {
+  console.log("The server is now running on port 3000")
 })
-
-
-app.listen(3000)
